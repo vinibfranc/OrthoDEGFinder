96c910b0d2e59a5404111ac8a75e4cc765343c20
vinibfranc <vinibfranc@gmail.com>
2019-09-16 14:44:11 -0300

    Update home texts and menus

--- project/app/views.py
+++ project/app/views.py
@@ -5,13 +5,15 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views import View
 from django.views.generic import ListView
 
+NUM_PAGES = 50
+
 def home(request):
     return render(request, 'home.html')
 
 def search_organism(request):
     organism_list = ExperimentalDesign.objects.all()
     organism_filter = OrganismDesignFilter(request.GET, queryset=organism_list)
-    paginator = Paginator(organism_filter.qs, 25)
+    paginator = Paginator(organism_filter.qs, NUM_PAGES)
     page = request.GET.get('page')
     page_organism = paginator.get_page(page)
     return render(request, 'filters/organism_list.html', {'filter': organism_filter, 'page_organism': page_organism})
@@ -19,7 +21,7 @@ def search_organism(request):
 def search_gene(request):
     gene_list = AnalysisAnnotatedGene.objects.all()
     gene_filter = AnalysisAnnotatedGeneFilter(request.GET, queryset=gene_list)
-    paginator = Paginator(gene_filter.qs, 25)
+    paginator = Paginator(gene_filter.qs, NUM_PAGES)
     page = request.GET.get('page')
     page_gene = paginator.get_page(page)
     return render(request, 'filters/gene_list.html', {'filter': gene_filter, 'page_gene': page_gene})
@@ -27,7 +29,7 @@ def search_gene(request):
 def search_annotation(request):
     annotation_list = Pannzer2Annotation.objects.all()
     annotation_filter = Pannzer2AnnotationFilter(request.GET, queryset=annotation_list)
-    paginator = Paginator(annotation_filter.qs, 25)
+    paginator = Paginator(annotation_filter.qs, NUM_PAGES)
     page = request.GET.get('page')
     page_annotation = paginator.get_page(page)
     return render(request, 'filters/annotation_list.html', {'filter': annotation_filter, 'page_annotation': page_annotation})
@@ -35,7 +37,7 @@ def search_annotation(request):
 def search_orthologs(request):
     orthologs_list = Ortholog.objects.all()
     orthologs_filter = OrthologFilter(request.GET, queryset=orthologs_list)
-    paginator = Paginator(orthologs_filter.qs, 25)
+    paginator = Paginator(orthologs_filter.qs, NUM_PAGES)
     page = request.GET.get('page')
     page_orthologs = paginator.get_page(page)
     return render(request, 'filters/orthologs_list.html', {'filter': orthologs_filter, 'page_orthologs': page_orthologs})
@@ -43,7 +45,7 @@ def search_orthologs(request):
 def search_unified(request):
     unified_list = GeneCorrespondences.objects.all()
     unified_filter = UnifiedFilter(request.GET, queryset=unified_list)
-    paginator = Paginator(unified_filter.qs, 25)
+    paginator = Paginator(unified_filter.qs, NUM_PAGES)
     page = request.GET.get('page')
     page_unified = paginator.get_page(page)
     return render(request, 'filters/unified_list.html', {'filter': unified_filter, 'page_unified': page_unified})
